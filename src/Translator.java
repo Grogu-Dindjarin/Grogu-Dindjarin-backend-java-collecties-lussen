44ba9c7e4f87ff8b2b3a8735afe1269b05a97697
Grogu-Dindjarin <grogu.dindjarin2@gmail.com>
2024-03-27 13:50:33 +0100

    Create Translator
    Finished Translator class

--- src/Translator.java
+++ src/Translator.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Translator {
+import java.util.HashMap;
+
+public class Translator {
+
+    private HashMap<Integer,String> numericAlpha;
+    private String[] alphabetic;
+    private Integer[] numeric;
+
+    public Translator(String[] alphabetic, Integer[] numeric) {
+        this.alphabetic = alphabetic;
+        this.numeric = numeric;
+
+        this.numericAlpha = new HashMap<>();
+
+        for (int i = 0; i < numeric.length; i++) {
+            this.numericAlpha.put(numeric[i], alphabetic[i]);
+        }
+    }
+
+    public String translate(Integer number) {
+        return this.numericAlpha.get(number);
+    }
 }
