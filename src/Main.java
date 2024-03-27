80e4284b92367fa69a7651942bf03a04ceee11c1
Grogu-Dindjarin <grogu.dindjarin2@gmail.com>
2024-03-27 13:52:36 +0100

    Add code to Main
    Created instance of Translator class
    Created a translation game with while loop
    
    Hence minimal requirements are met for the assignment

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,42 @@
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         // Hier mag je je code schrijven voor de hoofd-opdracht
+    Integer[] numeric = {1,2,3,4,5,6,7,8,9,0};
+    String[] alphabetic = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "nul"};
+    String invalid = "ongeldige invoer";
+    Scanner scanner = new Scanner(System.in);
 
+    Translator translator = new Translator(alphabetic, numeric);
+
+    boolean play = true;
+    while (play) {
+        System.out.println("Type 'x' om te stoppen \nType 'v' om te vertalen");
+        String input = scanner.nextLine();
+        if (Objects.equals(input, "x")) {
+            play = false;
+            scanner.close(); // Closing the scanner when terminating the program
+        } else if (Objects.equals(input, "v")) {
+            System.out.println("Type een cijfer in van 0 t/m 9");
+            int number;
+            try {
+                number = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println(invalid);
+                continue; // Skip to the next iteration of the loop
+            }
+
+            if (number >= 0 && number <= 9) {
+                String result = translator.translate(number);
+                System.out.println("De vertaling van " + number + " is " + result);
+            } else {
+                System.out.println(invalid);
+            }
+        }
+    }
 
     }
 }
